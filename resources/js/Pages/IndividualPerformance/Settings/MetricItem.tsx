e1578288629dcab48689501dc2f7f008cba9cede
Lhimor-Entino <60861718+Lhimor-Entino@users.noreply.github.com>
2024-07-23 15:52:10 +0800

    UPDATED SECONDS AND MINUTE ERROR

--- resources/js/Pages/IndividualPerformance/Settings/MetricItem.tsx
+++ resources/js/Pages/IndividualPerformance/Settings/MetricItem.tsx
@@ -12,13 +12,32 @@ interface Props {
 }
 
 const MetricItem:FC<Props> = ({metric,onEdit,onDelete}) => {
+
+    const isInSecondsOrMinutes = (unit:string,daily_goal:string) => {
+        let parts = daily_goal.split(':');
+
+        // Assuming the time string is always in the format HH:MM:SS
+        let hours = parseInt(parts[0], 10) || 0;
+        let minutes = parseInt(parts[1], 10) || 0;
+        let seconds = parseInt(parts[2], 10) || 0;
+        
+        let totalSeconds = hours * 3600 + minutes * 60 + seconds;
+        
+        let totalMinutes = parseInt(parts[1], 10) || 0;
+       
+        if(unit ==="Minutes") return totalMinutes;
+        if(unit ==="Seconds") return totalSeconds
+
+        return 0;
+    }
     return (
         <TableRow key={metric.id}>
             <TableCell className="font-medium">{metric.metric_name}</TableCell>
             <TableCell>{`${metric.user.first_name} ${metric.user.last_name}`}</TableCell>
             <TableCell className='capitalize'>{`${metric.format}`}</TableCell>
             <TableCell className='capitalize'>{`${metric.unit}`}</TableCell>
-            <TableCell>{parseInt(metric.daily_goal) < 1 ? "No Daily Goal" : metric.daily_goal}</TableCell>
+            {/* <TableCell>{  parseInt(metric.daily_goal) < 1 ? "No Daily Goal" : metric.daily_goal} {parseInt(metric.daily_goal)} {metric.daily_goal}</TableCell> */}
+            <TableCell>{ metric.unit === "Minutes" || metric.unit ==="Seconds" ?  isInSecondsOrMinutes(metric.unit || "",metric.daily_goal)  < 1 ? "No Daily Goal" : metric.daily_goal : parseInt(metric.daily_goal) < 1 ? "No Daily Goal" : metric.daily_goal}</TableCell>
             <TableCell className='flex items-center gap-x-2 justify-end'>
                 <Button size='icon' onClick={()=>onEdit(metric)}  variant='secondary'>
                     <PencilIcon />
